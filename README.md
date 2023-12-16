7976976c8f01f896b77addc52e311469901faa7f
chenweiqi <251712045@qq.com>
2023-12-16 17:42:22 +0800

    Revert "reduce lua calls, improve throughput, support blocking request"
    This reverts commit 983cc3debc6fdad4f73a320a5db6ec0e4235abbb.

--- README.md
+++ README.md
@@ -1,10 +1,10 @@
 # lua_httpsc
-An asynchronous https client library for lua
+An asynchronous https library for lua
 
 
 ## API
 ```lua
-httpsc = require "httpsc"
+local httpsc = require "httpsc"
 ```
 
 ### Connect remote https server
@@ -12,11 +12,7 @@ httpsc = require "httpsc"
 local ip = "127.0.0.1"
 local port = 443
 
-fd = httpsc.connect(ip, port)
-```
-
-### Check connection is ready, only for async
-```lua
+local fd = httpsc.connect(ip, port)
 -- Check connection
 while true do
 	local ok = httpsc.check_connect(fd)
@@ -24,26 +20,21 @@ while true do
 	httpsc.usleep(10000)
 end
 ```
-
 ### Write data to remote
 ```lua
+-- It is asynchronous now
 httpsc.send(fd, msg)
 ```
 
 ### Receive data from remote
 ```lua
-httpsc.recv(fd, size)       -- size is optional
+httpsc.recv(fd)
 ```
 
-
-### Set configure
+### Close Connection
 ```lua
-httpsc.set_conf({
-	init_lib = true, 		-- load openssl libary, default: true
-	async = true,			-- work under non-blocking, default: true
-	send_timeout = 3000,	-- socket send timeout, default: 3000 (3 second)
-	recv_timeout = 3000,	-- socket recv timeout, default: 3000 (3 second)
-})
+-- Actually, it's useless. Because close is auto executed by LUA GC
+httpsc.close(fd)
 ```
 
 ## Example
--- test.lua
+++ test.lua
@@ -1,7 +1,28 @@
 local httpsc = require "httpsc"
 local internal = require "internal"
 
--- async example
+local fd = httpsc.connect("104.193.88.77", 443)
+while true do
+	local ok = httpsc.check_connect(fd)
+	if ok then break end
+	httpsc.usleep(10000)
+end
+-- print(fd)
+local request = internal.request(httpsc, fd)
+local code, body = request("GET", "www.baidu.com", "/", nil, {}, nil)
+print("http code",code)
+print("body length",#body)
+print("done!")
+
+httpsc = nil
+request = nil
+package.loaded["httpsc"] = nil
+package.preload["httpsc"] = nil
+collectgarbage()
+collectgarbage()
+
+local httpsc = require "httpsc"
+
 local fd = httpsc.connect("104.193.88.77", 443)
 while true do
 	local ok = httpsc.check_connect(fd)
@@ -13,23 +34,3 @@ local code, body = request("GET", "www.baidu.com", "/", nil, {}, nil)
 print("http code",code)
 print("body length",#body)
 print("done!")
-
--- gc
-httpsc = nil
-request = nil
-package.loaded["httpsc"] = nil
-package.preload["httpsc"] = nil
-collectgarbage()
-collectgarbage()
-
-
--- sync example
-local httpsc = require "httpsc"
-httpsc.set_conf({async = false})
-local fd = httpsc.connect("104.193.88.77", 443)
-local request = internal.request(httpsc, fd)
-local code, body = request("GET", "www.baidu.com", "/", nil, {}, nil)
-print("http code",code)
-print("body length",#body)
-print("done!")
-
